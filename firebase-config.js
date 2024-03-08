a0f65ba6670a069ff970afc95e17e016b0c9be30
sarfraz764 <161279013+sarfraz764@users.noreply.github.com>
2024-03-08 00:16:39 -0800

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBO2sSDPGwZtDciE3g9ea2Yo1O7b-GkNXo",
-  authDomain: "bvf5tf-8hgtrf.firebaseapp.com",
-  databaseURL: "https://bvf5tf-8hgtrf-default-rtdb.firebaseio.com",
-  projectId: "bvf5tf-8hgtrf",
-  storageBucket: "bvf5tf-8hgtrf.appspot.com",
-  messagingSenderId: "123083150680",
-  appId: "1:123083150680:web:41a0d9243a8cbe0447412d"
+  apiKey: "AIzaSyD5hh-QRR6hBmNs42bESYF_1-iAJh-UuHQ",
+  authDomain: "self6edsew.firebaseapp.com",
+  databaseURL: "https://self6edsew-default-rtdb.firebaseio.com",
+  projectId: "self6edsew",
+  storageBucket: "self6edsew.appspot.com",
+  messagingSenderId: "404491758320",
+  appId: "1:404491758320:web:adac289c1bb1c7e5033035"
 };
